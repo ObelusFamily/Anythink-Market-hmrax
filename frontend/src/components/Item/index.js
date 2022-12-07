0d3abd4c9018066774a8c40073037ac1a8350d54
Lior Brauer <liorbrauer@gmail.com>
2022-12-07 12:27:24 +0200

    change to load as waterfall

--- frontend/src/components/Item/index.js
+++ frontend/src/components/Item/index.js
@@ -20,64 +20,90 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 class Item extends React.Component {
+  loadItemAndComments() {
+    agent.Items.get(this.props.match.params.id).then((item) => {
+      this.props.onLoad([item, []]);
+      agent.Comments.forItem(this.props.match.params.id).then((comments) => {
+        this.props.onLoad([item, comments]);
+      });
+    });
+  }
+
   componentWillMount() {
-    this.props.onLoad(
-      Promise.all([
-        agent.Items.get(this.props.match.params.id),
-        agent.Comments.forItem(this.props.match.params.id),
-      ])
-    );
+    this.loadItemAndComments();
   }
 
   componentWillUnmount() {
     this.props.onUnload();
   }
 
-  render() {
-    if (!this.props.item) {
-      return null;
-    }
-
-    const markup = {
-      __html: marked(this.props.item.description, { sanitize: true }),
-    };
-    const canModify =
+  canModify() {
+    return (
       this.props.currentUser &&
-      this.props.currentUser.username === this.props.item.seller.username;
+      this.props.currentUser.username === this.props.item.seller.username
+    );
+  }
+
+  render() {
     return (
       <div className="container page" id="item-container">
         <div className="text-dark">
           <div className="row bg-white p-4">
-            <div className="col-6">
-              <img
-                src={this.props.item.image}
-                alt={this.props.item.title}
-                className="item-img"
-                style={{ height: "500px", width: "100%", borderRadius: "6px" }}
-              />
-            </div>
-
-            <div className="col-6">
-              <h1 id="card-title">{this.props.item.title}</h1>
-              <ItemMeta item={this.props.item} canModify={canModify} />
-              <div dangerouslySetInnerHTML={markup}></div>
-              {this.props.item.tagList.map((tag) => {
-                return (
-                  <span className="badge badge-secondary p-2 mx-1" key={tag}>
-                    {tag}
-                  </span>
-                );
-              })}
-            </div>
+            {!this.props.item ? (
+              "Loading item..."
+            ) : (
+              <>
+                <div className="col-6">
+                  <img
+                    src={this.props.item.image}
+                    alt={this.props.item.title}
+                    className="item-img"
+                    style={{
+                      height: "500px",
+                      width: "100%",
+                      borderRadius: "6px",
+                    }}
+                  />
+                </div>
+                <div className="col-6">
+                  <h1 id="card-title">{this.props.item.title}</h1>
+                  <ItemMeta
+                    item={this.props.item}
+                    canModify={this.canModify()}
+                  />
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: marked(this.props.item.description, {
+                        sanitize: true,
+                      }),
+                    }}
+                  ></div>
+                  {this.props.item.tagList.map((tag) => {
+                    return (
+                      <span
+                        className="badge badge-secondary p-2 mx-1"
+                        key={tag}
+                      >
+                        {tag}
+                      </span>
+                    );
+                  })}
+                </div>
+              </>
+            )}
           </div>
 
           <div className="row bg-light-gray p-4">
-            <CommentContainer
-              comments={this.props.comments || []}
-              errors={this.props.commentErrors}
-              slug={this.props.match.params.id}
-              currentUser={this.props.currentUser}
-            />
+            {!this.props.comments ? (
+              "Loading comments..."
+            ) : (
+              <CommentContainer
+                comments={this.props.comments || []}
+                errors={this.props.commentErrors}
+                slug={this.props.match.params.id}
+                currentUser={this.props.currentUser}
+              />
+            )}
           </div>
         </div>
       </div>
