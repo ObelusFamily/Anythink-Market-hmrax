3f22566c77183d0b3166ac7acb017963690c5e84
Lior Brauer <liorbrauer@gmail.com>
2022-12-12 09:09:37 +0200

    go

--- frontend/src/components/Item/index.js
+++ frontend/src/components/Item/index.js
@@ -21,6 +21,7 @@ const mapDispatchToProps = (dispatch) => ({
 
 class Item extends React.Component {
   async componentDidMount() {
+    // wow
     const [item, comments] = await getItemAndComments(
       this.props.match.params.id
     );
